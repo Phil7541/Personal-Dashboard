a2c20d5c325b8b51c863d0c4ca9e9b1d466dbc4a
Phil7541 <phillipswann2008@gmail.com>
2026-06-12 22:34:24 +0100

    Sleep Update

--- src/sleep.py
+++ src/sleep.py
@@ -7,10 +7,10 @@ WAVESHARE_LIB = os.path.join(BASE_DIR, "lib")
 
 sys.path.append(WAVESHARE_LIB)
 
-from waveshare_epd import epd7in5
+from waveshare_epd import epd7in5_V2
 
 if __name__ == "__main__":
-    epd = epd7in5.EPD()
+    epd = epd7in5_V2.EPD()
     epd.init()
     epd.Clear()
     time.sleep(1)
